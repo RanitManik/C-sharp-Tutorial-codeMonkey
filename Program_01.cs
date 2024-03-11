dc038763825bdf288cc2b613e814a908dd3e4bce
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-03-11 21:52:54 +0530

    added project files

---
+++ Program_01.cs
@@ -0,0 +1,12 @@
+﻿// Hello World! program
+
+namespace c_sharp
+{
+    static class Program01
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
